0ac3ae3a1aa60465457727418d662cd7735992dc
kenziebourn <bournm@oregonstate.edu>
2024-10-14 15:15:11 -0700

    rotate user agents

--- py/scraper.py
+++ py/scraper.py
@@ -2,15 +2,22 @@ from bs4 import BeautifulSoup
 from data import EU_ALLERGENS,  REEF_HARM, NON_VEGAN, FUNGAL_ACNE
 import re
 import requests
+import random
 
 inci_dec = "https://incidecoder.com/ingredients/stearic-acid"
 ewg = "https://www.ewg.org/skindeep/ingredients/706311-STEARIC_ACID/"
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-}
+user_agents = [ 
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', 
+	'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36', 
+	'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148', 
+	'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36' 
+] 
+user_agent = random.choice(user_agents) 
+headers = {'User-Agent': user_agent} 
 
-result1 = requests.get(inci_dec)
+result1 = requests.get(inci_dec, headers=headers)
 result2 = requests.get(ewg, headers=headers)
 
 doc = BeautifulSoup(result1.text, "html.parser")
