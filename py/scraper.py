87370c91144a9abd54c14425548d17535695c43e
kenziebourn <bournm@oregonstate.edu>
2024-10-17 17:46:29 -0700

    test single ingredient

--- py/scraper.py
+++ py/scraper.py
@@ -1,11 +1,24 @@
 from bs4 import BeautifulSoup
 from data import EU_ALLERGENS,  REEF_HARM, NON_VEGAN, FUNGAL_ACNE
-import re
-import requests
-import random
+from ingredients import INGREDIENTS
+import re, requests, random, json, urllib.parse
 
-inci_dec = "https://incidecoder.com/ingredients/stearic-acid"
-ewg = "https://www.ewg.org/skindeep/ingredients/706311-STEARIC_ACID/"
+# Testing
+ingredient_name = 'STEARIC ACID'
+# print(ingredient_name)
+
+def create_url1(ingredient_name):
+    enc_inci = ingredient_name.replace(" ", "-").lower()
+    # Define the base URL for INCIDecoder
+    inci_dec = "https://incidecoder.com/ingredients/"
+    return inci_dec + enc_inci
+
+def create_url2(ingredient_name):
+    # Encode the ingredient name for the URL
+    encoded_name = urllib.parse.quote_plus(ingredient_name)
+    # Define the base URL for EWG
+    ewg = "https://www.ewg.org/skindeep/search/?search="
+    return ewg + encoded_name
 
 user_agents = [ 
 	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', 
@@ -17,40 +30,75 @@ user_agents = [
 user_agent = random.choice(user_agents) 
 headers = {'User-Agent': user_agent} 
 
-result1 = requests.get(inci_dec, headers=headers)
-result2 = requests.get(ewg, headers=headers)
+# Query search result for ingredients in Skin EWG
+def search_ewg():
+    url = create_url2(ingredient_name)
+    # Make the request to the URL
+    response = requests.get(url, headers=headers)
+
+    print("Response Code:", response.status_code)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    product_names = soup.find_all('div', class_='product-name')
+    # Use regex to match the ingredient name and extract the href attribute
+    pattern = re.compile(rf'\b{re.escape(ingredient_name)}\b', re.IGNORECASE)
+    for product in product_names:
+        product_text = product.get_text(strip=True)
+        if pattern.search(product_text):
+            href = product.find('a')['href']
+            # print(f"Matched Ingredient: {product_text}")
+            # print(f"Associated Href: {href}")
+            break  # Exit the loop after the first match
+
+    # Create the URL for the matched ingredient
+    url = f"https://www.ewg.org{href}"
+    # print("ewg url:", url)
+    return url
+
+link1 = create_url1(ingredient_name)
+link2 = search_ewg()
+# print(link2)
+
+result1 = requests.get(link1, headers=headers)
+result2 = requests.get(link2, headers=headers)
+# Check result status codes before proceeding
+if result1.status_code != 200:
+    print("Failed to fetch data from INCIDecoder")
+    exit()
+if result2.status_code != 200:
+    print("Failed to fetch data from EWG")
+    exit()
 
 doc = BeautifulSoup(result1.text, "html.parser")
 doc2 = BeautifulSoup(result2.text, "html.parser")
 
 # Get the name of the product
 name = doc.find(class_='detailpage').find('h1').text.strip()
-print(name)
+# print(name)
 
 # Get the irritancy of the ingredient
 irritancy = doc.find_all(string="Irritancy: ")
 parent = irritancy[0].parent
 child = irritancy[0].find_next("span")
-print(child.string.strip())
+# print(child.string.strip())
 
 # Get the comedogenicity of the ingredient
 comedogenicity = doc.find_all(string="Comedogenicity: ")
 parent = comedogenicity[0].parent
 child = comedogenicity[0].find_next("span")
-print(child.string.strip())
+# print(child.string.strip())
 
 # Get the description of the ingredient
 details_div = doc.find("div", id="details")
 p_tag = details_div.find("p")
 description = p_tag.text.strip()
-print(description)
+# print(description)
 
 # Get the safety rating of the ingredient
 safety = doc2.find_all('p', class_='data-level')
 if safety:
     safety_text = safety[0].text.strip()  # Get the text content
     data_level = safety_text.split(': ')[1]  # Split and get the second part
-    print(data_level)
+    # print(data_level)
 else:
     print("Safety data not found")
 
@@ -58,45 +106,49 @@ else:
 def is_alc_free(name):
     match = re.search(r'(?i)alcohol', name)
     if not match:
-        return("Alcohol Free")
+        return True
 
 def is_silicone_free(name):
     match = re.search(r'(?i)cone', name)
     if not match:
-        return("Silicone Free")
+        return True
 
 def is_fragrance_free(name):
     match = re.search(r'(?i)parfum', name)
     if not match:
-        return("Fragrance Free")
+        return True
 
 def is_sulfate_free(name):
     match = re.search(r'(?i)ate', name)
     if not match:
-        return("Sulfate Free")
+        return True
 
 def is_paraben_free(name):
     match = re.search(r'(?i)paraben', name)
     if not match:
-        return("Paraben Free")
+        return True
 
 def is_oil_free(name):
     match = re.search(r'(?i)oil', name)
     if not match:
-        return("Oil Free")
+        return True
     
-# print("NOTES:")
-print(is_alc_free(name))
-print(is_silicone_free(name))
-print(is_fragrance_free(name))
-print(is_sulfate_free(name))
-print(is_paraben_free(name))
-print(is_oil_free(name))
-if name.upper() not in EU_ALLERGENS:
-    print("EU Allergen Free")
-if name.upper() not in REEF_HARM:
-    print("Reef Safe")
-if name.upper() not in NON_VEGAN:
-    print("Vegan")
-if name.upper() not in FUNGAL_ACNE:
-    print("Fungal Acne Safe")
+ingredient_data = {
+    "name": name,
+    "irritancy": child.string.strip(),
+    "comodogenicity" : child.string.strip(),
+    "description": description,
+    "safety": data_level,
+    "alc_free": is_alc_free(name),
+    "silicone_free": is_silicone_free(name),
+    "fragrance_free": is_fragrance_free(name),
+    "sulfate_free": is_sulfate_free(name),
+    "paraben_free": is_paraben_free(name),
+    "oil_free": is_oil_free(name),
+    "eu_allergen": name.upper() not in EU_ALLERGENS,
+    "reef_safe": name.upper() not in REEF_HARM,
+    "vegan": name.upper() not in NON_VEGAN,
+    "fungal_acne_safe": name.upper() not in FUNGAL_ACNE
+}
+ingredient_json = json.dumps(ingredient_data, indent=4)
+print(ingredient_json)
