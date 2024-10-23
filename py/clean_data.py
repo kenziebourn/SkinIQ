c7a77f3b51e049eda0e5112c3d43af8b4f283fac
kenziebourn <bournm@oregonstate.edu>
2024-10-23 14:58:15 -0700

    further data cleansing

--- py/clean_data.py
+++ py/clean_data.py
@@ -1,4 +1,6 @@
-import csv, re
+
+## PART 1 PULLING INGREDIENTS FROM CSV FILE
+# import csv, re
 
 # def extract_ingredients_from_csv(csv_file):
 #     ingredients_list = []
@@ -28,15 +30,39 @@ import csv, re
 #     for ingredient in unique_ingredients:
 #         file.write(ingredient + '\n')
 
-# print(f"Unique ingredients have been written to {output_file}")
+# print(f"Part 1: Unique ingredients have been written to {output_file}")
 
-INGREDIENTS = []
-# Read the file and convert it to a list
-with open('unique_ingredients.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        INGREDIENTS.append(line.strip())
+# PART 2 CLEANING INGREDIENTS, REMOVING SENTENCES
+# import re 
+# filtered_lines = []
+# def process_ingredients_file(input_file, output_file):
+#     with open(input_file, 'r', encoding='utf-8') as file:
+#         for line in file:
+#             # Check if the length of the line is less than or equal to the max length
+#             if len(line.strip()) <= 45:
+#                 filtered_lines.append(line)
 
-# Print the resulting list
-print(INGREDIENTS)
 
+#     with open(output_file, 'w', encoding='utf-8') as file:
+#         file.writelines(filtered_lines)
+# input_file = 'processed_ingredients.txt'  # Replace with the path to your input file
+# output_file = 'processed_ingredients1.txt'  # Replace with the path to your output file
+# process_ingredients_file(input_file, output_file)
+# print(f"Part 2: Processed content has been written to {output_file}")
 
+# PART 3 CONVERT TO UPPERCASE, REMOVE REPEATS
+import re
+def clean_ingredients_file(input_file, output_file):
+    cleaned_lines = set()
+    with open(input_file, 'r', encoding='utf-8') as file:
+        for line in file:
+            # Convert the line to uppercase
+            cleaned_line = line.upper()
+            cleaned_lines.add(cleaned_line)
+
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.writelines(cleaned_lines)
+input_file = 'processed_ingredients1.txt'  
+output_file = 'processed_ingredients2.txt' 
+clean_ingredients_file(input_file, output_file)
+print(f"Part 3: Processed content has been written to {output_file}")
