e6b8dc8d1a92574ab4c509bf42f049b29c2dfdf5
kenziebourn <bournm@oregonstate.edu>
2024-10-23 17:28:14 -0700

    connect to mongo via py & load json

--- py/main.py
+++ py/main.py
@@ -1,17 +1,30 @@
-from fastapi import FastAPI
-from dotenv import dotenv_values
+import json, os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-config = dotenv_values(".env")
+load_dotenv()
+MONGO_DB_URI = os.getenv("MONGO_DB_URI")
+DB_NAME = os.getenv("DB_NAME")
+DB_COLLECTION = os.getenv("DB_COLLECTION")
 
-app = FastAPI()
+# Connect to MongoDB
+client = MongoClient(MONGO_DB_URI)
+print("Connected to MongoDB!")
+# print(client.list_database_names())
 
-@app.on_event("startup")
-def startup_db_client():
-    app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
-    print("Connected to the MongoDB database!")
+# Connect to the SkinIQ database
+db = client[DB_NAME]
+Collection = db[DB_COLLECTION]
 
-@app.on_event("shutdown")
-def shutdown_db_client():
-    app.mongodb_client.close()
+# Adapted from https://www.geeksforgeeks.org/how-to-import-json-file-in-mongodb-using-python/
+# Load the JSON data
+with open('output.json') as file:
+    file_data = json.load(file)
+     
+# Inserting the loaded data in the Collection
+# if JSON contains data more than one entry
+# insert_many is used else insert_one is used
+if isinstance(file_data, list):
+    Collection.insert_many(file_data)  
+else:
+    Collection.insert_one(file_data)
